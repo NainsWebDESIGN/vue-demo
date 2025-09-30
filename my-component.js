b3144a45b15d03eba274e43eb86e6fb1b29e6be6
Nains <jason20226@gmail.com>
2025-09-30 16:53:00 +0800

    2025-09-30-16-52

--- my-component.js
+++ my-component.js
@@ -6,11 +6,26 @@ export default {
             message: 'Hello World',
             isShow: true,
             x: 2,
+            y: 100,
             users: [
                 { name: 'Jake', email: 'jake@gmail.com' },
                 { name: 'Allen', email: 'allen@gmail.com' },
                 { name: 'Eason', email: 'eason@gmail.com' },
-            ]
+            ],
+            count: 0,
+            name: 'Jake'
         };
     },
+    methods: {
+        myFun(value) {
+            return this.x * this.y + value;
+        },
+        countEffect() {
+            this.count += 1;
+        },
+        nameEffect() {
+            this.isShow = !this.isShow;
+            this.name = (!this.isShow) ? 'Allen' : 'Jake';
+        }
+    }
 };
