c6ef5791d4531350799ae9e7d52231b6f43464db
Nains <jason20226@gmail.com>
2025-10-01 17:44:07 +0800

    8結束

--- my-component.js
+++ my-component.js
@@ -1,31 +1,32 @@
-import { ref } from 'vue';
+import { ref } from "vue";
 export default {
-    name: 'App',
-    data() {
-        return {
-            message: 'Hello World',
-            isShow: true,
-            x: 2,
-            y: 100,
-            users: [
-                { name: 'Jake', email: 'jake@gmail.com' },
-                { name: 'Allen', email: 'allen@gmail.com' },
-                { name: 'Eason', email: 'eason@gmail.com' },
-            ],
-            count: 0,
-            name: 'Jake'
-        };
+  name: "App",
+  data() {
+    return {
+      message: "Hello World",
+      isShow: true,
+      x: 2,
+      y: 100,
+      users: [
+        { name: "Jake", email: "jake@gmail.com" },
+        { name: "Allen", email: "allen@gmail.com" },
+        { name: "Eason", email: "eason@gmail.com" },
+      ],
+      count: 0,
+      name: "Jake",
+      inputText: "123",
+    };
+  },
+  methods: {
+    myFun(value) {
+      return this.x * this.y + value;
     },
-    methods: {
-        myFun(value) {
-            return this.x * this.y + value;
-        },
-        countEffect() {
-            this.count += 1;
-        },
-        nameEffect() {
-            this.isShow = !this.isShow;
-            this.name = (!this.isShow) ? 'Allen' : 'Jake';
-        }
-    }
+    countEffect() {
+      this.count += 1;
+    },
+    nameEffect() {
+      this.isShow = !this.isShow;
+      this.name = !this.isShow ? "Allen" : "Jake";
+    },
+  },
 };
